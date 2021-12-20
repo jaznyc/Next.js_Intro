1578f7aa0492939b945441b3e160186b17fa4280
jaznyc <jaznycdev@gmail.com>
2021-12-20 14:41:24 -0500

    updated next config file to read .env, used env variable in nav link

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const {
-  PHASE_PRODUCTION_BUILD,
-  PHASE_DEVELOPMENT_SERVER,
-} = require('next/constants');
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log('im in dev mode');
-    return defaultConfig;
-  }
-  return defaultConfig;
-};
+dotenvLoad();
+
+const withEnv = nextEnv();
+module.exports = withEnv();
