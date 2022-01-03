e064cc0bd8299368899e7ac463ac726dba1f7159
jaznyc <jaznycdev@gmail.com>
2022-01-03 18:01:59 -0500

    exploring dynamic server side rendering changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,17 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
+
+const BrowserComponent = dynamic(()=> import('../src/components/browser'), {ssr:false})
 
 export default ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
       <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
+      <BrowserComponent/>
     </div>
+
   </div>
 )
 
